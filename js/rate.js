20c77b21684c1c3bce664fc5797876de6e6980dd
afemenil <afemenil@gmail.com>
2022-09-12 08:22:49 +0100

    first commit

---
+++ js/rate.js
@@ -0,0 +1,39 @@
+// Calculate Function
+// Using the Simple Interest=p×r×t/100
+let print_interest = document.getElementById('interest');
+let print_roi = document.getElementById('roi');
+
+// Calculate Function
+const calculate = () =>{
+    let principal = document.getElementById('principal').value;
+    let rate = document.getElementById('rate').value;
+    let term = document.getElementById('duration').value;
+    let empty_message = document.getElementById('empty');
+
+    // If input is empty
+    if (principal == 0 && rate == 0 && term == 0) {
+        empty_message.innerHTML = "Try input number in the fields";
+    }
+    else{
+
+        // Simple interest Rate = p*r*t/100
+        let interest_rate = (principal*rate*term)/100;
+        let roi = parseFloat(principal) + interest_rate;
+
+        print_interest.textContent = '₦' + interest_rate.toFixed(2);
+        print_roi.textContent = '₦' + roi.toFixed(2);
+
+    }
+    //Reset function
+    const reset =()=> {
+        alert('Reset');
+    }
+}
+
+
+// Reset Function
+// const reset = () =>{
+//     document.getElementById('form').reset();
+//     print_interest.textContent = '';
+//     print_roi.textContent = '';  
+// }
